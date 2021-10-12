15eeae42d3db40c65898e53a15d534e967dfafb9
Varsha Chary <34280450+varshachary@users.noreply.github.com>
2021-10-12 11:10:08 -0400

    Update Python.py

--- Python.py
+++ Python.py
@@ -1,1 +1,14 @@
-
+'''
+Write a python code to find if entries to a list have duplicate characters. What is the computation complexity of the code?
+'''
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        i = 0
+        while i < len(nums) - 1:
+            if nums[i] == nums[i+1]:
+                del nums[i]
+            else:
+                i += 1
+        return len(nums)
+        
+#alternatively use set. 
